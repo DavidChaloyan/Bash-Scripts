04d44811de5abfa32462b5e5beb29ac7f64abaaf
DavidChaloyan <87094339+DavidChaloyan@users.noreply.github.com>
2021-08-30 15:22:13 +0400

    Update bright.sh

--- bright.sh
+++ bright.sh
@@ -1,6 +1,6 @@
 changeBrightness () {
 echo "Please enter a brightness level in range 0 to 255 "
-cat >> /sys/class/backlight/amdgpu_bl0/brightness
+sudo cat >> /sys/class/backlight/amdgpu_bl0/brightness
 }
 changeBrightness
 
